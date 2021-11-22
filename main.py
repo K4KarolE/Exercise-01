06eb4891b8cf09073f5d0d58699873acc6cbef23
K4Karole <karoly.egyed@protonmail.com>
2021-11-22 18:35:08 +0000

    Calc

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-weight = input("What is your weight? ")
-weight_type = input("Is it pound(LBS) or KG?\nType `L` if it is LBS and type `K` if is KG ")
+weight = float(input("What is your weight? "))
+weight_type = input("KG or LBS? Type `K` or `L`. ")
 
-if weight_type =
+if weight_type.upper() != "L" and weight_type.upper() != "K":
+    print("Sorry, invalid mass type")
+if weight_type.upper() == "L":
+    print("Your weight in KGs: " + str(weight * 0.45))
+if weight_type.upper() == "K":
+    print("Your weight in LBs: " + str(weight / 0.45))
 
 
